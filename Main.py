8d159fe738c8d5781edb24ef209cbb6ec165825b
Rendya Yuschak <rendyayuschak@gmail.com>
2019-10-30 15:09:36 +0700

    new task.py

---
+++ Main.py
@@ -0,0 +1,7 @@
+"""
+This is the main class
+"""
+from DataTask.Task import TaskN
+print("Successfully Started")
+
+belajarMD = TaskN("Belajar Matdas","2 Januari 2019",4,"4 Hour")
---
+++ TaskClass.py/taskClass.py
@@ -0,0 +1,10 @@
+"""
+"""
+import datetime
+
+class Task(object):
+    """docstring for Task."""
+
+    def __init__(self, name, start, span, end, deadline, important,):
+        super(Task, self).__init__()
+        self.arg = arg
