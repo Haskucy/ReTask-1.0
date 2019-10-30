5b13f4c53b2fca3e48e22b59d56ab2949d55b87d
Rendya Yuschak <rendyayuschak@gmail.com>
2019-10-30 21:34:11 +0700

    get set

--- TaskClass.py/taskClass.py
+++ TaskClass.py/taskClass.py
@@ -2,9 +2,51 @@
 """
 import datetime
 
-class Task(object):
+class Task():
     """docstring for Task."""
 
-    def __init__(self, name, start, span, end, deadline, important,):
-        super(Task, self).__init__()
-        self.arg = arg
+    def __init__(self, name, span, deadline, startline=datetime.datetime.now):
+        self.name = name
+        self.span = span
+        self.deadline=deadline
+        self.startline =startline
+        self.start = None
+        self.end = None
+
+    # Semua method Get() Set()
+    
+    def getName(self):
+        return self.name
+
+    def setName(self, name):
+        self.name = name
+
+    def getSpan(self):
+        return self.span
+
+    def setSpan(self, span):
+        self.span = span
+
+    def getDeadline(self):
+        return self.deadline
+
+    def setDeadline(self, deadline):
+        self.deadline = deadline
+
+    def getStartline(self):
+        return self.startline
+
+    def setStartline(self, startline):
+        self.startline = startline
+
+    def getStart(self):
+        return self.start
+
+    def setStart(self, start):
+        self.start = start
+
+    def getEnd(self):
+        return self.end
+
+    def setEnd(self, end):
+        self.end = end
